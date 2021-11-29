6eb80501fb1316902d12e4aaa17a190e65b90f2a
DagoTorres <dagotorres26@outlook.com>
2021-11-29 13:20:20 -0600

    api
    api

---
+++ key.php
@@ -0,0 +1,6 @@
+<?php
+$host = 'http://id.wa-server.com' ;    
+$token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIxNSwiaWF0IjoxNjM3MzY4NjQ1LCJleHAiOjE2Njg5MjYyNDV9.MK2NuMm2x6rFd28Lmh5xsfdTYDpkLVKYy0-ElH8C0rc' ;
+$deviceid = '79' ; 
+$devicekey = 'da652c1f-f0cb-4b79-a640-340ebf30bbe4' ;
+?>
